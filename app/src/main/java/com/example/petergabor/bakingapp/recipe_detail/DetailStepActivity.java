93c9e7fb2094ebc573e87379edc013e484906bca
Tupir <gaborpeter11@gmail.com>
2017-08-31 14:49:13 +0200

    exoplayer implementation

--- app/src/main/java/com/example/petergabor/bakingapp/recipe_detail/DetailStepActivity.java
+++ app/src/main/java/com/example/petergabor/bakingapp/recipe_detail/DetailStepActivity.java
@@ -1,6 +1,8 @@
 package com.example.petergabor.bakingapp.recipe_detail;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +10,18 @@ import android.support.v7.widget.RecyclerView;
 import com.example.petergabor.bakingapp.R;
 import com.example.petergabor.bakingapp.recipe_descripton.RecipeDescriptionAdapter;
 import com.example.petergabor.bakingapp.utils.Recept;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.ArrayList;
 
@@ -18,6 +32,8 @@ public class DetailStepActivity extends AppCompatActivity {
     private RecipeDescriptionAdapter mAdapter;
     ArrayList<String> shortDescriptionArray;
     int position;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +57,55 @@ public class DetailStepActivity extends AppCompatActivity {
 
         System.out.println(position);
 
+        // Initialize the player view.
+        mPlayerView = (SimpleExoPlayerView) findViewById(R.id.playerView);
+        initializePlayer(Uri.parse("asset:///clair_de_lune.mp3"));
 
 
     }
+
+
+    /**
+     * Initialize ExoPlayer.
+     * @param mediaUri The URI of the sample to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(this, "BakingApp");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    this, userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+
+    /**
+     * Release the player when the activity is destroyed.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+
+
+
 }
